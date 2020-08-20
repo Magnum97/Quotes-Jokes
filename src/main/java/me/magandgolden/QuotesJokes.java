a00ac906663cfadbf7961afd07908bcccd04c892
Richard Simpson <magnum1997@gmail.com>
2020-08-20 00:35:57 -0700

    Add resource files and code

--- src/main/java/me/magandgolden/QuotesJokes.java
+++ src/main/java/me/magandgolden/QuotesJokes.java
@@ -1,7 +1,31 @@
 package me.magandgolden;
 
-import co.aikar.commands.BukkitCommandManager;
+import de.leonhard.storage.Yaml;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuotesJokes extends JavaPlugin {
+
+	public void onEnable () {
+		createConfigs();
+	}
+
+	private void createConfigs () {
+		// This will create "quotes.yml" in the plugins' folder.
+		Yaml quoteFile = new Yaml("quotes", getDataFolder().toString(), getResource("quotes.yml"));
+		Yaml jokeFile = new Yaml("jokes", getDataFolder().toString(), getResource("jokes.yml"));
+
+
+		// When making object like list, or Set you need to specify what will be in the list/set
+		// with < > around it.
+
+		// Note two ways to do this. You can do it on two lines
+		List <String> quoteList = new ArrayList <>();
+		quoteList = quoteFile.getStringList("quotes");
+
+		// or combine it and do on one line.
+		List <String> jokeList = jokeFile.getStringList("jokes");
+	}
 }
