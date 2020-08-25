de19da5e4749fbaab4300053fb43c770c1a36495
Richard Simpson <magnum1997@gmail.com>
2020-08-24 22:44:20 -0700

    Started sorting methods

--- src/main/java/me/magandgolden/MainCommand.java
+++ src/main/java/me/magandgolden/MainCommand.java
@@ -2,12 +2,13 @@ package me.magandgolden;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Optional;
 import co.aikar.commands.annotation.Subcommand;
 import de.leonhard.storage.Yaml;
 import org.bukkit.command.CommandSender;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @CommandAlias ("random")
@@ -16,52 +17,65 @@ public class MainCommand extends BaseCommand {
 	private static final QuotesJokes plugin = QuotesJokes.getPlugin();
 
 	@Subcommand ("joke")
-	private static void onJoke (CommandSender sender, @Optional String action, @Optional String[] arg2) {
-		Yaml jokes = plugin.getJokeFile();
-		if (action != null) {
-			if (action.equals("add")) {
-				if (arg2.length < 1) { // If no joke is added, send message
-					sender.sendMessage("I need to know what to add.");
-					sender.sendMessage("Use: /random joke add <joke>");
-				}
-				// if joke is added to command, add to file.
-				jokes.getStringList("jokes").add(Arrays.toString(arg2));
-				jokes.write();
-				sender.sendMessage("Your joke was added!");
-				return;
-			}
-			if (action.equals("list")) {
-				for (int i = 0; i < jokes.getStringList("jokes").size(); i++) {
-					sender.sendMessage((i + 1) + ": " + jokes.getStringList("jokes").get(i));
-				}
-			return;
-			}
-			int JokeCount = plugin.getJokeFile().getStringList("jokes").size();
-			int choose = ThreadLocalRandom.current().nextInt(JokeCount);
-			sender.sendMessage(plugin.getJokeFile().getStringList("jokes").get(choose));
+	private static void onJoke (CommandSender sender, @Default ("send") String action, @Default ("") String[] arg2) {
+		Yaml jokeFile = plugin.getJokeFile();
+		List <String> jokes = plugin.getJokeFile().getStringList("jokes");
+		switch (action) {
+			case "send":
+				sendRandom(sender, jokes);
+				break;
+			case "list":
+				sendList(sender, jokes);
+				break;
+			case "add":
+				addList(sender, jokes, arg2);
+				break;
+			case "remove":
+				int number = Integer.parseInt(arg2.toString());
+				removeList(sender, jokes, number);
 		}
+
 	}
 
 	@Subcommand ("quote")
-	private static void onQuote (CommandSender sender, @Optional String action, @Optional String[] arg2) {
-		Yaml quotes = plugin.getQuoteFile();
-		if (action != null) {
-			if (action.equals("add")) {
-				if (arg2.length < 1) {
-					sender.sendMessage("I need to know what to add.");
-					sender.sendMessage("Use: /random quote add <quote>");
-				}
-				quotes.getStringList("quotes").add(Arrays.toString(arg2));
-				quotes.write();
-				sender.sendMessage("Your quote was added!");
-				return;
-			}
-			int QuoteCount = plugin.getQuoteFile().getStringList("quotes").size();
-			int choose = ThreadLocalRandom.current().nextInt(QuoteCount);
-			sender.sendMessage(plugin.getQuoteFile().getStringList("quotes").get(choose));
+	private static void onQuote (CommandSender sender, @Optional String action, @Optional String arg2) {
+		List <String> quotes = plugin.getQuoteFile().getStringList("quotes");
+		if (action.isEmpty()) {
+			sendRandom(sender, quotes);
 		}
+		int QuoteCount = (int) plugin.getQuoteFile().getStringList("quotes").size();
+		int choose = ThreadLocalRandom.current().nextInt(QuoteCount);
+		sender.sendMessage(plugin.getQuoteFile().getStringList("quotes").get(choose));
 	}
 }
+
+	private static void addList (CommandSender sender, List <String> list, String[] array) {
+		StringBuilder builder = new StringBuilder();
+		for (String string : array) {
+			builder.append(string);
+		}
+		String add = builder.toString();
+		list.add(add);
+		sender.sendMessage(add + " has been added");
+	}
+
+	private static void sendList (CommandSender sender, List <String> list) {
+		for (int i = 0;i<list.size();i++){
+			sender.sendMessage(i+": "+list.get(i));
+		}
+
+	}
+
+	private static void removeList (CommandSender sender, List <String> list, int i) {
+sender.sendMessage("Removed #"+i+": "+list.get(i-1));
+
+	}
+
+	private static void sendRandom (CommandSender sender, List <String> list) {
+		int jokeCount = list.size();
+		int choice = ThreadLocalRandom.current().nextInt(jokeCount);
+		sender.sendMessage(list.get(choice));
+	}
 
 /*
 	@Default // Leave this for later
