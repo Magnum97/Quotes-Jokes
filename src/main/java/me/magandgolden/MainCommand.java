b3c1dbad6818b934a56d9f13f5ab30c2a57414d8
GoldenSecrets <raerae734@gmail.com>
2020-08-25 02:39:06 -0400

    Added comments. Small formatting changes
    Testing

--- src/main/java/me/magandgolden/MainCommand.java
+++ src/main/java/me/magandgolden/MainCommand.java
@@ -2,7 +2,6 @@ package me.magandgolden;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Optional;
 import co.aikar.commands.annotation.Subcommand;
 import de.leonhard.storage.Yaml;
@@ -16,27 +15,29 @@ public class MainCommand extends BaseCommand {
 
 	private static QuotesJokes plugin = QuotesJokes.getPlugin();
 
-	@Subcommand("joke")
-	private static void onJoke(CommandSender sender, @Optional String action, @Optional String[] arg2) {
+	@Subcommand ("joke")
+	private static void onJoke (CommandSender sender, @Optional String action, @Optional String[] arg2) {
 		Yaml jokes = plugin.getJokeFile();
 		if (action != null) {
 			if (action.equals("add")) {
-				if (arg2.length < 1) {
+				if (arg2.length < 1) { // If no joke is added, send message
 					sender.sendMessage("I need to know what to add.");
 					sender.sendMessage("Use: /random joke add <joke>");
 				}
+				// if joke is added to command, add to file.
 				jokes.getStringList("jokes").add(Arrays.toString(arg2));
 				jokes.write();
+					sender.sendMessage("Your joke was added!");
 				return;
 			}
-			int JokeCount = (int) plugin.getJokeFile().getStringList("jokes").size();
+			int JokeCount = plugin.getJokeFile().getStringList("jokes").size();
 			int choose = ThreadLocalRandom.current().nextInt(JokeCount);
 			sender.sendMessage(plugin.getJokeFile().getStringList("jokes").get(choose));
 		}
 	}
 
-	@Subcommand("quote")
-	private static void onQuote(CommandSender sender, @Optional String action, @Optional String[] arg2) {
+	@Subcommand ("quote")
+	private static void onQuote (CommandSender sender, @Optional String action, @Optional String[] arg2) {
 		Yaml quotes = plugin.getQuoteFile();
 		if (action != null) {
 			if (action.equals("add")) {
@@ -46,12 +47,15 @@ public class MainCommand extends BaseCommand {
 				}
 				quotes.getStringList("quotes").add(Arrays.toString(arg2));
 				quotes.write();
+					sender.sendMessage("Your quote was added!");
 				return;
 			}
-			int QuoteCount = (int) plugin.getQuoteFile().getStringList("quotes").size();
+			int QuoteCount = plugin.getQuoteFile().getStringList("quotes").size();
 			int choose = ThreadLocalRandom.current().nextInt(QuoteCount);
 			sender.sendMessage(plugin.getQuoteFile().getStringList("quotes").get(choose));
 		}
+	}
+}
 
 /*
 	@Default // Leave this for later
@@ -59,5 +63,4 @@ public class MainCommand extends BaseCommand {
 
 	}
 */
-	}
-}
+
